d1f3cf2635a5ec3ddb754060b54caa1d96eb05d8
afzal hamdulay <afzalhamdulay1@gmail.com>
2025-01-06 20:26:10 +0400

    added netlify url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,10 +5,11 @@ import axios from 'axios';
 
 // Set the base URL for Axios
 // const url = process.env.REACT_APP_API_URL || "http://localhost:4000";
+const url = "https://677c032440396b7b1ac3afbe--ecom1-mern.netlify.app/"
 
 const api = axios.create({
   // baseURL: `https://mern-ecom-6pp1-wg85-git-main-afzals-projects-6b32063b.vercel.app/api/v1`,
-  baseURL: `/api/v1`,
+  baseURL: `${url}/api/v1`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
