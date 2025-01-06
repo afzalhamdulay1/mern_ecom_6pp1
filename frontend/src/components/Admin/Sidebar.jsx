5063404ffbf744120aa6c5f97e34cacd341aab73
afzal hamdulay <afzalhamdulay1@gmail.com>
2025-01-06 17:17:19 +0400

    sidebar.jsx change

--- frontend/src/components/Admin/Sidebar.jsx
+++ frontend/src/components/Admin/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./sidebar.css";
+import "./Sidebar.css";
 import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
 import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
